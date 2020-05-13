93a6184b009392f4202fb60e029c0e0a06a11d10
Roland McRae <mcraeroland@gmail.com>
2020-05-13 14:11:03 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(title, genre)
+    song = Song.new(title, genre)
+    song.artist = self
+  end
+
+end
