b0cb0f34f354f194844f81cd119dc04c0ceeac77
Roland McRae <mcraeroland@gmail.com>
2020-05-13 23:44:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,8 @@ class Artist
     @@all
   end
 
-  def new_song(name, artist, genre)
-    Song.new(name, artist, genre)
-
+  def new_song(name, genre)
+    Song.new(name, self, genre)
   end
 
   def songs
